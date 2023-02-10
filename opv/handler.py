82a4a6adbfea4bb8811570c38aa154e1d86d9840
B.H.J. Thate <operbot100@gmail.com>
2023-02-10 22:25:37 +0100

    add handler and storage

---
+++ opv/handler.py
@@ -0,0 +1,230 @@
+# This file is placed in the Public Domain.
+
+
+import inspect
+import queue
+import threading
+
+
+from opv.default import Default
+from opv.objects import Object, register, update
+
+
+def __dir__():
+    return (
+            "Event",
+            "Handler",
+            "Listens",
+            "Thread",
+            "launch",
+            "scan"
+           ) 
+
+
+__all__ = __dir__()
+
+
+class Handler(Object):
+
+    cmds = Object()
+    errors = []
+
+    def __init__(self):
+        Object.__init__(self)
+        self.cbs = Object()
+        self.queue = queue.Queue()
+        self.stopped = threading.Event()
+        register(self.cbs, "command", self.dispatch)
+        Listens.add(self)
+
+    def dispatch(self, event):
+        if not event.isparsed:
+            event.parse(event.txt)
+        if not event.orig:
+            event.orig = repr(self)
+        func = getattr(Handler.cmds, event.cmd, None)
+        if func:
+            try:
+                func(event)
+            except Exception as ex:
+                exc = ex.with_traceback(ex.__traceback__)
+                Handler.errors.append(exc)
+                event.ready()
+                return None
+            event.show()
+        #event.ready()
+
+    def handle(self, event):
+        func = getattr(self.cbs, event.type, None)
+        if not func:
+            event.ready()
+            return
+        func(event)
+
+    def loop(self):
+        while not self.stopped.set():
+            self.handle(self.poll())
+
+    def poll(self):
+        return self.queue.get()
+
+    def put(self,event):
+        if not event.orig:
+            event.orig = repr(self)
+        self.queue.put_nowait(event)
+
+    def register(self, typ, cbs):
+        setattr(self.cbs, typ, cbs)
+
+    def stop(self):
+        self.stopped.set()
+
+    def start(self):
+        self.stopped.clear()
+        self.loop()
+
+
+class Listens(Object):
+
+    objs = []
+
+    @staticmethod
+    def add(obj):
+        if repr(obj) not in [repr(x) for x in Listens.objs]:
+            Listens.objs.append(obj)
+
+    @staticmethod
+    def announce(txt):
+        for obj in Listens.objs:
+            obj.announce(txt)
+
+    @staticmethod
+    def byorig(orig):
+        res = None
+        for obj in Listens.objs:
+            if repr(obj) == orig:
+                res = obj
+                break
+        return res
+
+    @staticmethod
+    def say(orig, txt, channel=None):
+        bot = Listens.byorig(orig)
+        if bot:
+            if channel:
+                bot.say(channel, txt)
+            else:
+                bot.raw(txt)
+
+
+class Event(Default):
+
+    def __init__(self):
+        Default.__init__(self)
+        self.__ready__ = threading.Event()
+        self.__thr__ = None
+        self.args = []
+        self.gets = Object()
+        self.isparsed = False
+        self.result = []
+        self.sets = Object()
+        self.type = "command"
+        self.toskip = Object()
+
+    def parsed(self):
+        return self.isparsed
+
+    def parse(self, txt):
+        self.isparsed = True
+        self.otxt = txt
+        spl = self.otxt.split()
+        args = []
+        _nr = -1
+        for word in spl:
+            if word.startswith("-"):
+                try:
+                    self.index = int(word[1:])
+                except ValueError:
+                    self.opts = self.opts + word[1:2]
+                continue
+            try:
+                key, value = word.split("==")
+                if value.endswith("-"):
+                    value = value[:-1]
+                    setattr(self.toskip, value, "")
+                setattr(self.gets, key, value)
+                continue
+            except ValueError:
+                pass
+            try:
+                key, value = word.split("=")
+                setattr(self.sets, key, value)
+                continue
+            except ValueError:
+                pass
+            _nr += 1
+            if _nr == 0:
+                self.cmd = word
+                continue
+            args.append(word)
+        if args:
+            self.args = args
+            self.rest = " ".join(args)
+            self.txt = self.cmd + " " + self.rest
+        else:
+            self.txt = self.cmd
+
+    def ready(self):
+        self.__ready__.set()
+
+    def reply(self, txt):
+        self.result.append(txt)
+
+    def show(self):
+        for txt in self.result:
+            Listens.say(self.orig, txt, self.channel)
+
+    def wait(self):
+        self.__ready__.wait()
+
+
+class Thread(threading.Thread):
+
+    def __init__(self, func, thrname, *args, daemon=True):
+        super().__init__(None, self.run, thrname, (), {}, daemon=daemon)
+        self._result = None
+        self.name = thrname or name(func)
+        self.queue = queue.Queue()
+        self.queue.put_nowait((func, args))
+        self.sleep = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        for k in dir(self):
+            yield k
+
+    def join(self, timeout=None):
+        super().join(timeout)
+        return self._result
+
+    def run(self) -> None:
+        func, args = self.queue.get()
+        self._result = func(*args)
+
+
+def launch(func, *args, **kwargs):
+    thrname = kwargs.get("name", func.__name__)
+    thr = Thread(func, thrname, *args)
+    thr.start()
+    return thr
+
+
+def scan(mod):
+    for key, cmd in inspect.getmembers(mod, inspect.isfunction):
+        if key.startswith("cb"):
+            continue
+        names = cmd.__code__.co_varnames
+        if "event" in names:
+            setattr(Handler.cmds, key, cmd)
